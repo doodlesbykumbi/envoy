97fbf0399956ca8660866d676f04cbe2160ff209
Kumbirai Tanekha <kumbirai.tanekha@gmail.com>
2019-02-12 01:55:10 +0800

    clearer demo instructions

--- secretless-demo/run.sh
+++ secretless-demo/run.sh
@@ -1,20 +1,45 @@
 # build envoy
-bazel build //source/exe:envoy --verbose_failures
+pushd ..
+    bazel build //source/exe:envoy --verbose_failures
+popd
 
 # symlink shared library in anticipation for dynamic linking
 ln -s $PWD/source/extensions/filters/network/mysql_proxy/secretless.a secretless.a
 
 # start envoy. set file secret provider and provide configure to speak to mysql:3306 on 9903
-SECRET_PROVIDER=file ../bazel-bin/source/exe/envoy-static --config-yaml "$(cat config-mysql.yml)"
+# also specify dynamic linking path for secretlesslib
+LD_LIBRARY_PATH=$PWD/../source/extensions/filters/network/mysql_proxy
+  DYLD_LIBRARY_PATH=$LD_LIBRARY_PATH \
+  SECRET_PROVIDER=file \
+  ../bazel-bin/source/exe/envoy-static --config-yaml "$(cat config-mysql.yml)"
 
 # start mysql
 docker run --name mysql-test --rm -e MYSQL_ROOT_PASSWORD=securerootpass -p 3306:3306 -d mysql/mysql-server:5.7
+
+#wait for mysql
+docker exec -it mysql-test  bash -c "
+echo 'Waiting for MySQL to start'
+while ! mysqladmin -psecurerootpass status > /dev/null 2>&1;
+do
+  >&2 printf '. '
+  sleep 1
+done
+
+echo ''
+>&2 echo 'MySQL is up - continuing'
+"
 
 # create a user that can login using password from any host
 docker exec -it mysql-test mysql -uroot -psecurerootpass -e "CREATE USER 'kumbi'@'%' IDENTIFIED BY 'password'; GRANT ALL PRIVILEGES ON * . * TO 'kumbi'@'%';"
 
 # access mysql via envoy using secret provider (notice client is given gibberish credentials)
 docker exec -it mysql-test mysql -h host.docker.internal -P 9903 --ssl=FALSE -u kxmbi -ppxssword -e "status"
+
+# inspect the container logs and see that secretless is in the mix:
+# [2019-02-12 01:51:03.516][1295323][info][filter] [source/extensions/filters/network/mysql_proxy/mysql_filter.cc:38] requestingAuth
+# 2019/02/12 01:51:03 Instantiating provider 'file'
+# 2019/02/12 01:51:03 Instantiating provider 'file'
+#
 
 # modify db-username or db-password
 printf "wrong-password" > db-password
